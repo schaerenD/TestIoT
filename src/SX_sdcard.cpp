136564e95492e974d542e77641dcbe73f9f25e8a
Schären Daniel <Daniel.Schaeren@swisscom.com>
2022-09-09 09:09:25 +0200

    Datem Managment

--- src/SX_sdcard.cpp
+++ src/SX_sdcard.cpp
@@ -13,7 +13,7 @@ void sdcard_init()
         while (1);
     }
 
-    File f = SD.open("/TEST.txt", FILE_READ);
+    File f = SD.open("/CONFIGURATION.txt", FILE_READ);
     if (f) {
         Test = f.readString();
         display_debug_output(Test);
@@ -28,20 +28,21 @@ void sdcard_init()
     // Init SD Card
     int firstParameterBegin = Test.indexOf('=');
     int firstParameterEnd = Test.indexOf('\r');
+    int secondParameterBegin = Test.indexOf('=',firstParameterEnd);
+    int secondParameterEnd = Test.indexOf('\r',secondParameterBegin);
+    int thirdParameterBegin = Test.indexOf('=',secondParameterEnd);
+    int thirdParameterEnd = Test.indexOf('\r',thirdParameterBegin);
+
     //int secondParameter =  Test.indexOf(',');
     //int thirdParameter =  Test.indexOf(',');
-    
-    String firstParameter = Test.substring(firstParameterBegin, firstParameterEnd);
-    String firstParameter = Test.substring(firstParameterBegin, firstParameterEnd);
-    String firstParameter = Test.substring(firstParameterBegin, firstParameterEnd);
-    String firstParameter = Test.substring(firstParameterBegin, firstParameterEnd);
+
+    String firstParameter = Test.substring(firstParameterBegin+1, firstParameterEnd);
+    String secondParameter = Test.substring(secondParameterBegin+1, secondParameterEnd);
+    String thirdParameter = Test.substring(thirdParameterBegin+1, thirdParameterEnd);
+
     display_debug_output(firstParameter);
-    display_debug_output(firstParameter);
-    display_debug_output(firstParameter);
-    display_debug_output(firstParameter);
-    display_debug_output(firstParameter);
-    display_debug_output(firstParameter);
+    display_debug_output(secondParameter);
+    display_debug_output(thirdParameter);
 
     delay(10000);
-    
 }
